f507da513b034fde1e6f4a71e9e8047e71f91b76
fuminopen <fuminopen@gmail.com>
2024-04-29 19:58:18 +0900

    LSPの設定

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 local options = {
   backup = false, -- creates a backup file
   clipboard = "unnamedplus", -- allows neovim to access the system clipboard
