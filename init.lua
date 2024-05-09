6cb9638790d138546927777581f7f12064b9eafa
fuminopen <fuminopen@gmail.com>
2024-05-10 00:04:58 +0900

    neovim上でもWindowsのクリップボードを利用できるように

--- init.lua
+++ init.lua
@@ -4,3 +4,29 @@ require("plugins.nvim-cmp")
 require("plugins.nvim-lspconfig")
 require("plugins.github-copilot")
 require("plugins.lazygit")
+
+-- set clipboard to unnamedplus --
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") == 1 then
+  if vim.fn.executable("wl-copy") == 0 then
+    print("wl-clipboard not found, clipboard integration won't work")
+  else
+    vim.g.clipboard = {
+      name = "wl-clipboard (wsl)",
+      copy = {
+        ["+"] = "wl-copy --foreground --type text/plain",
+        ["*"] = "wl-copy --foreground --primary --type text/plain",
+      },
+      paste = {
+        ["+"] = function()
+          return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { "" }, 1) -- '1' keeps empty lines
+        end,
+        ["*"] = function()
+          return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { "" }, 1)
+        end,
+      },
+      cache_enabled = true,
+    }
+  end
+end
