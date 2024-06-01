b13ee395fd8e193a5b31f89051abef74574d33e4
fuminopen <fuminopen@gmail.com>
2024-06-02 00:36:36 +0900

    隠しファイルが見れるように

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -64,6 +64,11 @@ return {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignore = false,
+        },
       },
       window = {
         mappings = {
