ddc1af566afbb8d37c44d04a2c6b541c6f25c9f5
fuminopen <fuminopen@gmail.com>
2024-06-01 16:40:02 +0900

    init.luaの自動的に読み込まれるものを削除

--- init.lua
+++ init.lua
@@ -1,12 +1,1 @@
 require("config.lazy")
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-
-require("plugins.github-copilot")
-require("plugins.lspsaga")
-require("plugins.nvim-cmp")
-require("plugins.nvim-lspconfig")
-require("plugins.toggleterm")
-require("plugins.tree-sitter")
-require("plugins.winresizer")
