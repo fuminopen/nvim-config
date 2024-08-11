0d2ebbfbdec2bdc4a769af36a0ef06b1131292b1
fuminopen <fuminopen@gmail.com>
2024-08-11 20:27:42 +0900

    prettierを追加してvueをフォーマットさせる

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -50,9 +50,9 @@ return {
           lua = { "stylua" },
           fish = { "fish_indent" },
           sh = { "shfmt" },
-          vue = { "eslint_d" }, -- VueファイルのフォーマットにESLintを使用
-          javascript = { "eslint_d" },
-          typescript = { "eslint_d" },
+          vue = { "prettier" }, -- VueファイルのフォーマットにESLintを使用
+          javascript = { "prettier" },
+          typescript = { "prettier" },
         },
         -- The options you set here will be merged with the builtin formatters.
         -- You can also define any custom formatters here.
