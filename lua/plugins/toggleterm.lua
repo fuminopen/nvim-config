2da0406b8812e9d743717a601cd34ee314945ea8
fuminopen <fuminopen@gmail.com>
2025-04-14 15:41:19 +0900

    toggletermを設定を変えて再インストール

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = {
+      open_mapping = [[<c-n>]],
+      insert_mappings = true,
+      terminal_mappings = true,
+      direction = "float",
+    },
+  },
+}
